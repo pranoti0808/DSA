cd66261d0e213d134200d213c80361c1ebf4d466
Pranoti <153119495+pranoti0808@users.noreply.github.com>
2025-11-30 16:40:50 +0530

    Add files via upload

--- Linear_Search.cpp
+++ Linear_Search.cpp
@@ -1,4 +1,36 @@
 #include<iostream>
+#include<vector>
+using namespace std;
+
+int LinearSearch(vector<int>arr, int key){
+    for(int i = 0; i<arr.size(); i++){
+        if(arr[i] == key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int>arr1 = {1,3,2,4,23,11,5};
+    int key = 3;
+
+    int index = LinearSearch(arr1, key);
+    cout<< "Element Found at index: "<< index;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+#include<iostream>
 using namespace std;
 
 int LinearSearch(int arr[],int n,int key){
@@ -23,3 +55,4 @@ int main(){
 
     return 0;
 }
+*/
