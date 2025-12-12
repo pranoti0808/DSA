806b892145b1b11dc9f517995cd447987d943e97
Pranoti <153119495+pranoti0808@users.noreply.github.com>
2025-12-12 17:01:32 +0530

    Add files via upload

---
+++ InOrder.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x){
+    data = x;
+    left = right = NULL;
+    }
+};
+
+
+void InOrder(Node* node, vector<int>&res){
+    if(node == nullptr)
+        return;
+
+    InOrder(node->left, res);
+    res.push_back(node->data);
+    InOrder(node->right,res);
+}
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+
+    vector<int> res;
+    InOrder(root, res);
+
+    for(int node: res){
+        cout<<node<<" ";
+    }
+
+    return 0;
+
+
+}
